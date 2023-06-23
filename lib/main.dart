3814ef70731bd93cffdf8324607310ee9bfb5fa0
Hassan <hasanahmad9812021@gmail.com>
2023-06-23 22:29:26 +0600

    donefghjkl;'

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
+void main(){ /// code
+  runApp(MyApp()); /// entry point of apps
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePage(), /// start the task of screen
+      
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<dynamic> posts = [];
-  int currentPage = 1;
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchPosts();
-  }
-
-  Future<void> fetchPosts() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    final response = await http.get(
-      Uri.parse(
-          'https://jsonplaceholder.typicode.com/posts?_start=${(currentPage - 1) * 10}&_limit=10'),
-    );
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      setState(() {
-        posts.addAll(data);
-        isLoading = false;
-      });
-    } else {
-      throw Exception('Failed to fetch posts, try again');
-    }
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  Future<void> loadMorePosts() async {
-    currentPage++;
-    await fetchPosts();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Posts'),
-        centerTitle: true,
+        title: Text('Home Screen'),
+        backgroundColor: Colors.blueGrey,
+        actions: [
+         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
+         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
+         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
+         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: posts.length + 1,
-        itemBuilder: (context, index) {
-          if (index == posts.length) {
-            if (isLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              return Container(
-                margin: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: () => loadMorePosts(),
-                    child: const Text('Load More'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.redAccent)
+                ),
+                height: 200,
+                width: 200,
+                //color: Colors.redAccent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                          'https://media.licdn.com/dms/image/C4D03AQGp4j5K-UUilg/profile-displayphoto-shrink_800_800/0/1656577164812?e=1692835200&v=beta&t=B5gpRNbnyG_u-oEWtAwd2jvXQwemALoI4Q0Dk1_Ig2g', height: 120, width: 120 ,),
+                      SizedBox(height: 20,),
+                      Text(
+                        'Software Engineer',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              );
-            }
-          }
-
-          final post = posts[index];
-
-          return Card(
-            margin: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Text(post['id'].toString(), style: const TextStyle(fontWeight: FontWeight.w800),),
-                    const SizedBox(width: 20,),
-                    Text(post['title'], style: const TextStyle(fontWeight: FontWeight.bold),),
-                  ],
-                ),
+                  alignment: Alignment.center,
               ),
-
-              subtitle: Text(post['body']),
             ),
-          );
-        },
-      ),
+          ),
+        ],
+      )
+
+
+
+
     );
   }
 }
+
+
+/// Scaffold -> individual screen
