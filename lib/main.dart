062d1fec76ea1148187709870707212aef6f1307
Hassan <hasanahmad9812021@gmail.com>
2023-06-24 22:46:13 +0600

    donefghjkl;'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){ /// code
-  runApp(MyApp()); /// entry point of apps
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(), /// start the task of screen
-      
+      home: HomePage(),
     );
   }
 }
@@ -24,56 +24,25 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Screen'),
-        backgroundColor: Colors.blueGrey,
-        actions: [
-         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
-         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
-         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
-         IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
-        ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.redAccent)
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
                 height: 200,
                 width: 200,
-                //color: Colors.redAccent,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.network(
-                          'https://media.licdn.com/dms/image/C4D03AQGp4j5K-UUilg/profile-displayphoto-shrink_800_800/0/1656577164812?e=1692835200&v=beta&t=B5gpRNbnyG_u-oEWtAwd2jvXQwemALoI4Q0Dk1_Ig2g', height: 120, width: 120 ,),
-                      SizedBox(height: 20,),
-                      Text(
-                        'Software Engineer',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  alignment: Alignment.center,
-              ),
-            ),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.redAccent, width: 8)),
+                child: Text('Container'),
+              )
+            ],
           ),
-        ],
-      )
-
-
-
-
+        ),
+      ),
     );
   }
 }
-
-
-/// Scaffold -> individual screen
